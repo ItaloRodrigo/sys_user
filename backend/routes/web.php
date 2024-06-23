685579db0f10d72d92180f997ecd19a73687ff4b
Ítalo Rodrigo <italo.computation@gmail.com>
2024-06-22 20:12:19 -0400

    Frontend
    - Instalação do framework vue.js
    - Instalação de dependências do vue.js

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        "mensagem" => 'Seja bem vindo ao sys_user',
+    ], 200);
 });
